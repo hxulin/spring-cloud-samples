eed13fac806831ca4607b45f6171d5d0214591c4
Castle <1455936807@qq.com>
2019-01-30 11:11:42 +0800

    集成 Swagger2

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 **Spring Cloud 的演示项目**
 
-| 模块                 | 说明                       |
-| -------------------- | -------------------------- |
-| app-parent           | Maven 父项目，统一依赖管理 |
-| app-common-interface | 公共接口定义               |
-| eureka-server        | 服务注册与发现模块         |
-| app-platform         | 平台微服务                 |
-| app-book             | 图书微服务                 |
+| 模块                 | 说明                               |
+| -------------------- | ---------------------------------- |
+| app-parent           | Maven 父项目，统一各模块的版本管理 |
+| app-common-interface | 公共接口定义                       |
+| eureka-server        | 服务注册与发现模块                 |
+| app-platform         | 平台微服务                         |
+| app-book             | 图书微服务                         |
 
 **项目启动说明**
 
@@ -18,14 +18,13 @@
 
    平台微服务地址为：`http://localhost:7900/`
 
+   访问平台微服务的 API 接口测试页面：`http://localhost:7900/swagger-ui.html`
+
 3. 启动图书微服务 app-book
 
    图书微服务地址为：`http://localhost:7910/`
 
-4. 访问图书微服务 `cn.huangxulin.cloud.controller.BookController` 相关方法，将会发起对平台微服务的调用
+   访问图书微服务的 API 接口测试页面：`http://localhost:7910/swagger-ui.html`
 
-5. ...
+   **测试图书微服务 BookController 的相关接口时，图书微服务会发起对平台微服务的调用**
 
-**待扩展**
-
-集成 Swagger2
---
+++ app-platform/src/main/java/cn/huangxulin/cloud/Swagger2.java
@@ -0,0 +1,48 @@
+package cn.huangxulin.cloud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 功能描述:
+ *
+ * @author hxulin
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    // Swagger2 的配置文件, 这里可以配置 Swagger2 的一些基本的内容, 比如扫描的包等等
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                // 为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("cn.huangxulin.cloud.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    // 构建 Api 文档的详细信息函数, 注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 页面标题
+                .title("平台微服务 REST API 测试")
+                // 创建人
+                .contact(new Contact("HuangXuLin", "https://huangxulin.cn/", null))
+                // 版本号
+                .version("1.0")
+                // 描述
+                .description("API 描述")
+                .build();
+    }
+}
